dae0d823ce9186e231a2669eab12040c4263325b
Timonso <management@timonso.de>
2026-04-12 17:16:04 +0200

    chore: update repository URL and relax plugin version in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,11 @@ rootProject.name = "surf-moderation-tools"
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://repo.slne.dev/repository/maven-public/") { name = "maven-public" }
+        maven("https://reposilite.slne.dev/releases")
     }
 }
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("dev.slne.surf.api.gradle.settings") version "26.1+"
+    id("dev.slne.surf.api.gradle.settings") version "+"
 }
